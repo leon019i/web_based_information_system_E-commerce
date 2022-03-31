e3497295a7b5880c91aaff6be9f479b2e09c9484
elhusseinn <elhusseinabdalaziz1@gmail.com>
2022-03-31 19:36:30 +0200

    adding login in & logout functionalilty to home page

--- E-commerce/store/controller/authview.py
+++ E-commerce/store/controller/authview.py
@@ -3,6 +3,7 @@ from multiprocessing import context
 import re
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
 from store.forms import CustomUserForm
 
 def register(request):
@@ -18,4 +19,31 @@ def register(request):
     return render (request, "store/auth/register.html", context)
 
 def loginpage(request):
-    return render (request, "store/auth/login.html")
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect('/')
+
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=name, password=password) 
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "logged in successfully!")
+
+                return redirect("/")
+            else:
+                messages.error(request, "invalid username or password")
+                return redirect("/login")
+
+        return render (request, "store/auth/login.html")
+
+
+def logoutpage(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "logged out successfully!")
+    return redirect("/")
