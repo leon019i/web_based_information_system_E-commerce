a180aa8dcb26609fbcad36e396b39ff96ab6476c
Mark77645 <Marksamirf@gmail.com>
2022-05-29 20:40:44 +0200

    Fixed bugs

--- E-commerce/store/controller/order.py
+++ E-commerce/store/controller/order.py
@@ -6,19 +6,22 @@ from store.models import Order,OrderItem
 
 def index(request):
     orders = Order.objects.filter(user = request.user)
-    productnames = OrderItem.objects.raw('SELECT * FROM store_product JOIN store_orderitem ON store_product.id=store_orderitem.product_id JOIN store_order ON store_orderitem.order_id = store_order.id WHERE store_order.user_id = %s ',[request.user.id])
+    productnames = OrderItem.objects.raw('SELECT * FROM store_product JOIN store_orderitem ON store_product.id = store_orderitem.product_id JOIN store_order ON store_orderitem.order_id = store_order.id WHERE store_order.user_id = %s ',[request.user.id])
     productnamesarray = []
     productpricesarray = []
-    productprices = Order.objects.raw('SELECT * FROM  store_order  WHERE store_order.user_id = %s ',[request.user.id])
+    orderstrackingnumberarray = []
+    orderdates = []
+    status = []
     for x in productnames:
-        if(x.name in productnamesarray):
-            continue
-        else:
-            productnamesarray.append(x.name)
-    for y in productprices:
-        productpricesarray.append(y.total_price)
-    zipped = zip(orders,productnames)
-    context = {'orders' : orders,'productnamesarray':productnamesarray,'productnames':productnames,'zipped':zipped,'productpricesarray':productpricesarray}
+        productnamesarray.append(x.name)
+        productpricesarray.append(x.price)
+        orderstrackingnumberarray.append(x.tracking_no)
+        orderdates.append(x.created_at)
+        status.append(x.status)
+    # for y in productprices:
+    #     productpricesarray.append(y.price)
+    zipped = zip(productnamesarray,productpricesarray,orderstrackingnumberarray,orderdates,status)
+    context = {'productnamesarray':productnamesarray,'zipped':zipped,'productpricesarray':productpricesarray}
     return render(request,"store/orders/index.html",context)
 
 def vieworder(request, t_no):
